35e75a55ffd0372efd0c8acf50717bb18b7c85a6
unknown <cskeith01@gmail.com>
2022-03-08 10:25:35 +0100

    Add properties and a constructor for class Beverage

--- index.php
+++ index.php
@@ -8,10 +8,19 @@ error_reporting(E_ALL);
 
 // EXERCISE 1
 //TODO: Create a class beverage.
-class beverage =
+class Beverage 
 {
 //TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
+    public string $color;
+    public float  $price;
+    public string $temperature;
 
+    public function __construct($color, $price, $temperature)
+    {
+        $this->$color = $color;
+        $this->$price = $price;
+        $this->$temperature = $temperature;
+    }
 }
 
 //TODO: Have a default value "cold" in the construct for temperature.
@@ -19,5 +28,5 @@ class beverage =
 //TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
 //TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold //automatically
 // print the getInfo on the screen.
-TODO: Print the temperature on the screen.
+//TODO: Print the temperature on the screen.
 //USE TYPEHINTING EVERYWHERE!
