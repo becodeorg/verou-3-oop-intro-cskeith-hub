66b5fae46015be2351903887b15fba78be9afd6f
unknown <cskeith01@gmail.com>
2022-03-08 11:26:22 +0100

    Start exercise 2

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ class beverage
     //TODO: Have a default value "cold" in the construct for temperature.
     public string $temperature;
 
-    public function __construct( $color,  $price,  $temperature="cold")
+    public function __construct(string $color, float $price, string $temperature="cold")
     {
         $this->color = $color;
         $this->price = $price;
@@ -25,15 +25,54 @@ class beverage
 
     //Remember for now we will use properties and methods that can be accessed from everywhere.
     //TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
+
     public function getInfo()
     {
-        echo "This beverage is {$this->temperature} and {$this->color}.";
+        echo "This beverage is {$this->temperature} and {$this->color}."."<br>";
     }
 }
 //TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold //automatically
+
 $cola = new beverage("black", 2);
 
 // print the getInfo on the screen.
-$cola->getInfo();
 //TODO: Print the temperature on the screen.
-//USE TYPEHINTING EVERYWHERE!
+//USE TYPE HINTING EVERYWHERE!
+$cola->getInfo();
+
+// EXERCISE 2
+// TODO: Make class beer that extends from Beverage.
+class beer
+{
+    // TODO: Create the properties name (string) and alcoholPercentage (float).
+    public string $name;
+    public float $alcoholPercentage;
+
+    // TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholpercentage.
+    // Remember for now we will use properties and methods that can be accessed from everywhere.
+    public function __construct(string $name, float $alcoholPercentage)
+    {  
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+    // TODO: Make a getAlcoholPercentage function which returns the alocoholPercentage.
+    // TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
+    public function getAlcoholPercentage()
+    {
+        echo "This is {$this->name} and contains {$this->alcoholPercentage} % Alcohols "."<br>";
+    }
+}
+// TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+$duval = new beer("Duval", 8.5);
+
+$duval->getAlcoholPercentage();
+
+
+
+
+
+
+// Make sure that each print is on a different line.
+// Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
+// USE TYPE HINTING EVERYWHERE!
+
