c07fa1017909a57600596d3b01a0b7bbcf24b590
unknown <cskeith01@gmail.com>
2022-03-08 10:39:10 +0100

    Add getInfo funtion

--- index.php
+++ index.php
@@ -8,25 +8,30 @@ error_reporting(E_ALL);
 
 // EXERCISE 1
 //TODO: Create a class beverage.
-class Beverage 
+class beverage 
 {
 //TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
     public string $color;
     public float  $price;
-    public string $temperature;
+    //TODO: Have a default value "cold" in the construct for temperature.
+    public string $temperature = "cold";
 
     public function __construct($color, $price, $temperature)
     {
-        $this->$color = $color;
-        $this->$price = $price;
-        $this->$temperature = $temperature;
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    //Remember for now we will use properties and methods that can be accessed from everywhere.
+    //TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
+    public function getInfo()
+    {
+        echo "This beverage is $this->temperature and $this->color.";
     }
 }
-
-//TODO: Have a default value "cold" in the construct for temperature.
-//Remember for now we will use properties and methods that can be accessed from everywhere.
-//TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
 //TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold //automatically
+$beverage1 = new beverage("black", 2, "");
 // print the getInfo on the screen.
 //TODO: Print the temperature on the screen.
 //USE TYPEHINTING EVERYWHERE!
